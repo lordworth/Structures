d22b52c6a97ea3cbb7110a8a5e0046cd93cf028a
Ds <deusdedit.lutwama@gmail.com>
2018-07-01 10:06:05 +0300

    added traverse method in stack

--- src/implementations/Stack.java
+++ src/implementations/Stack.java
@@ -1,6 +1,6 @@
 package implementations;
 //Stack interface to be used by stack class
-
+//this is another commit
 public interface Stack<T> {
     public void push(T element);
     public T peek();
